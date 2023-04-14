8e0f377803beaacbed520862360c52c76e86a52d
Leonardo Vaz <87207693+leowvazd@users.noreply.github.com>
2023-04-14 14:21:58 -0300

    project done

--- projeto_perguntas/lib/main.dart
+++ projeto_perguntas/lib/main.dart
@@ -1,32 +1,59 @@
 import 'package:flutter/material.dart';
-import './questao.dart';
-import './resposta.dart';
+import './questionario.dart';
+import './resultado.dart';
 
 void main() => runApp(const PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
+  var _pontuacaoTotal = 0;
   final _perguntas = const [
     {
       'texto': 'Qual é a sua cor favorita?',
-      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+      'respostas': [
+        {'texto': 'Preto', 'pontuacao': 10},
+        {'texto': 'Vermelho', 'pontuacao': 8},
+        {'texto': 'Verde', 'pontuacao': 6},
+        {'texto': 'Branco', 'pontuacao': 4},
+        {'texto': 'Rosa', 'pontuacao': 2},
+      ],
     },
     {
       'texto': 'Qual é o seu animal favorito?',
-      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+      'respostas': [
+        {'texto': 'Coelho', 'pontuacao': 10},
+        {'texto': 'Cobra', 'pontuacao': 8},
+        {'texto': 'Elefante', 'pontuacao': 6},
+        {'texto': 'Leão', 'pontuacao': 4},
+        {'texto': 'Peixe', 'pontuacao': 2},
+      ],
     },
     {
       'texto': 'Qual é o seu instrutor favorito?',
-      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
-    },
+      'respostas': [
+        {'texto': 'Leo', 'pontuacao': 10},
+        {'texto': 'Maria', 'pontuacao': 8},
+        {'texto': 'João', 'pontuacao': 6},
+        {'texto': 'Pedro', 'pontuacao': 4},
+        {'texto': 'Zé', 'pontuacao': 2},
+      ],
+    }
   ];
 
-  void _responder() {
+  void _responder(int pontuacao) {
     if (temPerguntaSelecionada) {
       setState(() {
         _perguntaSelecionada++;
+        _pontuacaoTotal += pontuacao;
       });
     }
+  }
+
+  void _reiniciarQuestionario() {
+    setState(() {
+      _perguntaSelecionada = 0;
+      _pontuacaoTotal = 0;
+    });
   }
 
   bool get temPerguntaSelecionada {
@@ -35,23 +62,18 @@ class _PerguntaAppState extends State<PerguntaApp> {
 
   @override
   Widget build(BuildContext context) {
-    List<String> respostas = temPerguntaSelecionada
-        ? _perguntas[_perguntaSelecionada]['respostas'] as List<String>
-        : [];
-
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Perguntas'),
         ),
         body: temPerguntaSelecionada
-            ? Column(
-                children: [
-                  Questao(_perguntas[_perguntaSelecionada]['texto'] as String),
-                  ...respostas.map((t) => Resposta(t, _responder)).toList(),
-                ],
+            ? Questionario(
+                perguntas: _perguntas,
+                perguntaSelecionada: _perguntaSelecionada,
+                quandoResponder: _responder,
               )
-            : null,
+            : Resultado(_pontuacaoTotal, _reiniciarQuestionario),
       ),
     );
   }
---
+++ projeto_perguntas/lib/resultado.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  final int pontuacao;
+  final void Function() quandoReiniciarQuestionario;
+
+  const Resultado(this.pontuacao, this.quandoReiniciarQuestionario,
+      {super.key});
+
+  String get fraseResultado {
+    if (pontuacao < 5) {
+      return 'Parabéns';
+    } else if (pontuacao < 15) {
+      return 'Você é bom!';
+    } else if (pontuacao < 25) {
+      return 'Impressionante!';
+    } else {
+      return 'Nível Jedi!';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            fraseResultado,
+            style: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+        ),
+        Center(
+          child: Text(
+            'Pontuação: ${pontuacao.toString()}',
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+        ),
+        TextButton(
+          onPressed: quandoReiniciarQuestionario,
+          child: const Text(
+            'Reiniciar?',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
